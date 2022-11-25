dd69c0711cdbbe1043329a8d1f0b19f7723a087a
ryanf10 <ryanfernaldy10@gmail.com>
2022-11-25 16:22:33 +0700

    feat: face detection

--- app/src/main/java/com/example/emotiondetection/CameraActivity.java
+++ app/src/main/java/com/example/emotiondetection/CameraActivity.java
@@ -2,6 +2,7 @@ package com.example.emotiondetection;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.SurfaceView;
@@ -10,20 +11,61 @@ import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Collections;
 import java.util.List;
 
 public class CameraActivity extends org.opencv.android.CameraActivity {
     private static String LOGTAG = "OpenCV_Log";
     private CameraBridgeViewBase cameraBridgeViewBase;
-    private int cameraIndex = 0;
+    private CascadeClassifier cascadeClassifier;
+    private int cameraIndex = 1;
 
     private BaseLoaderCallback baseLoaderCallback = new BaseLoaderCallback(this) {
         @Override
         public void onManagerConnected(int status) {
             switch (status){
                 case BaseLoaderCallback.SUCCESS:{
+                    InputStream is = getResources().openRawResource(R.raw.haarcascade_frontalface_default);
+                    File cascadeDir = getDir("cascade", Context.MODE_PRIVATE);
+                    File cascadeFile = new File(cascadeDir, "haarcascade_frontalface_default.xml");
+
+                    try {
+                        FileOutputStream fos = new FileOutputStream(cascadeFile);
+                        byte[] buffer = new byte[4096];
+                        int bytesRead;
+
+                        while((bytesRead = is.read(buffer)) != -1){
+                            fos.write(buffer, 0, bytesRead);
+                        }
+
+                        is.close();
+                        fos.close();
+
+                        cascadeClassifier = new CascadeClassifier(cascadeFile.getAbsolutePath());
+
+                        if (cascadeClassifier.empty()){
+                            cascadeClassifier = null;
+                        }else{
+                            cascadeDir.delete();
+                        }
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }catch (IOException e){
+                        e.printStackTrace();
+                    }
+
                     Log.d(LOGTAG, "OpenCV Loaded");
                     cameraBridgeViewBase.enableView();
                     break;
@@ -65,6 +107,19 @@ public class CameraActivity extends org.opencv.android.CameraActivity {
         @Override
         public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
             Mat input_rgba = inputFrame.rgba();
+            Mat input_gray = inputFrame.gray();
+
+            MatOfRect faceDetection = new MatOfRect();
+            cascadeClassifier.detectMultiScale(input_gray, faceDetection, 1.3, 5);
+
+            for (Rect rect: faceDetection.toArray()) {
+                Imgproc.rectangle(input_rgba,
+                        new Point(rect.x, rect.y - 50),
+                        new Point(rect.x + rect.width, rect.y + rect.height + 10),
+                        new Scalar(255, 0, 0),
+                        2);
+            }
+
             return input_rgba;
         }
     };
